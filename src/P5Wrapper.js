eadd551c869fdbd5fd3b928100c878f87e8cce1d
eaurhon <aurelien.hontabat@ericsson.com>
2017-12-22 14:01:31 +0100

    fix: #13 performance degradation

--- src/P5Wrapper.js
+++ src/P5Wrapper.js
@@ -12,12 +12,16 @@ export default class P5Wrapper extends React.Component {
 
   componentWillReceiveProps(newprops) {
     if(this.props.sketch !== newprops.sketch){
-      this.wrapper.removeChild(this.wrapper.childNodes[0]);
+      this.canvas.remove();
       this.canvas = new p5(newprops.sketch, this.wrapper);
     }
     if( this.canvas.myCustomRedrawAccordingToNewPropsHandler ) {
       this.canvas.myCustomRedrawAccordingToNewPropsHandler(newprops);
     }
+  }
+
+  componentWillUnmount() {
+    this.canvas.remove();
   }
 
   render() {
