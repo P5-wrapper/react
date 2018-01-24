28f55449e07b03b5f00504b1908835bda4a9f659
Shirley <s.lin12@gmail.com>
2018-01-24 09:06:53 -0500

    easy cleanup when component unmounts

--- src/P5Wrapper.js
+++ src/P5Wrapper.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import p5 from 'p5';
+import React from "react";
+import p5 from "p5";
 
 export default class P5Wrapper extends React.Component {
+	componentDidMount() {
+		this.canvas = new p5(this.props.sketch, this.wrapper);
+		if (this.canvas.myCustomRedrawAccordingToNewPropsHandler) {
+			this.canvas.myCustomRedrawAccordingToNewPropsHandler(this.props);
+		}
+	}
 
-  componentDidMount() {
-    this.canvas = new p5(this.props.sketch, this.wrapper);
-    if( this.canvas.myCustomRedrawAccordingToNewPropsHandler ) {
-      this.canvas.myCustomRedrawAccordingToNewPropsHandler(this.props);
-    }
-  }
+	componentWillReceiveProps(newprops) {
+		if (this.props.sketch !== newprops.sketch) {
+			this.wrapper.removeChild(this.wrapper.childNodes[0]);
+			this.canvas = new p5(newprops.sketch, this.wrapper);
+		}
+		if (this.canvas.myCustomRedrawAccordingToNewPropsHandler) {
+			this.canvas.myCustomRedrawAccordingToNewPropsHandler(newprops);
+		}
+	}
 
-  componentWillReceiveProps(newprops) {
-    if(this.props.sketch !== newprops.sketch){
-      this.wrapper.removeChild(this.wrapper.childNodes[0]);
-      this.canvas = new p5(newprops.sketch, this.wrapper);
-    }
-    if( this.canvas.myCustomRedrawAccordingToNewPropsHandler ) {
-      this.canvas.myCustomRedrawAccordingToNewPropsHandler(newprops);
-    }
-  }
+	componentWillUnmount() {
+		this.canvas.remove();
+	}
 
-  render() {
-    return <div ref={wrapper => this.wrapper = wrapper}></div>;
-  }
+	render() {
+		return <div ref={wrapper => (this.wrapper = wrapper)} />;
+	}
 }
