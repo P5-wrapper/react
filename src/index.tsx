f34923e160bdbf7ad2e9944a2c4b46478c6fdc83
Andreas Wolf <andreas.wolf@diva-e.com>
2019-05-28 17:36:14 +0200

    Destroy Canvas if Component unmounts/changes sketch

--- src/index.tsx
+++ src/index.tsx
@@ -21,12 +21,17 @@ export default class P5Wrapper extends Component <P5WrapperProps, {}> {
   componentWillReceiveProps(newprops: { sketch: (...args: any[]) => any; }) {
     if(this.props.sketch !== newprops.sketch){
       this.wrapper.removeChild(this.wrapper.childNodes[0]);
+      this.canvas.remove();
       this.canvas = new p5(newprops.sketch, this.wrapper);
     }
     if( this.canvas.myCustomRedrawAccordingToNewPropsHandler ) {
       this.canvas.myCustomRedrawAccordingToNewPropsHandler(newprops);
     }
   }
+
+  componentWillUnmount() {
+		this.canvas.remove();
+	}
 
   render() {
     return <div ref={wrapper => this.wrapper = wrapper}></div>;
