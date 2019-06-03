64377f5ea4a26c37998869a1e196b746ffe5e3ea
Andreas Wolf <andreas.wolf@diva-e.com>
2019-06-03 09:44:46 +0200

    removed dom and sound addons

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import { Component } from 'react';
 import p5 = require('p5');
-require('p5/lib/addons/p5.dom');
-require('p5/lib/addons/p5.sound');
 import React = require('react');
 
 export interface P5WrapperProps {
