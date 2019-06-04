6e1dacfedc5652da05202e3be27f07bfb751c751
Andreas Wolf <andreas.wolf@diva-e.com>
2019-06-04 11:52:20 +0200

    #release 2.0.0 refactored to use Derived State from Props Function to be comform with react 16.3.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,62 @@
 import { Component } from 'react';
 import p5 = require('p5');
 import React = require('react');
+import { stat } from 'fs';
 
 export interface P5WrapperProps {
   sketch: (p: p5) => void;
 }
-
-export default class P5Wrapper extends Component <P5WrapperProps, {}> {
-
+export interface P5WrapperState {
+  sketch: (p: p5) => void;
   canvas: p5;
+  wrapper: HTMLElement;
+}
+
+export default class P5Wrapper extends Component <P5WrapperProps, P5WrapperState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state  = {
+      sketch: props.sketch,
+      canvas: null,
+      wrapper: null
+    };
+  }
+
   wrapper: HTMLElement;
 
   componentDidMount() {
-    this.canvas = new p5(this.props.sketch, this.wrapper);
-    if( this.canvas.myCustomRedrawAccordingToNewPropsHandler ) {
-      this.canvas.myCustomRedrawAccordingToNewPropsHandler(this.props);
+    const canvas = new p5(this.state.sketch, this.wrapper);
+    if( canvas.myCustomRedrawAccordingToNewPropsHandler ) {
+      canvas.myCustomRedrawAccordingToNewPropsHandler(this.props);
     }
+    this.setState({
+      canvas: canvas,
+      wrapper: this.wrapper
+    })
   }
 
-  componentWillReceiveProps(newprops: { sketch: (...args: any[]) => any; }) {
-    if(this.props.sketch !== newprops.sketch){
-      this.wrapper.removeChild(this.wrapper.childNodes[0]);
-      this.canvas.remove();
-      this.canvas = new p5(newprops.sketch, this.wrapper);
+  static getDerivedStateFromProps(props: P5WrapperProps, state: P5WrapperState){
+    let canvas = state.canvas;
+    if(state.sketch !== props.sketch){
+      state.wrapper.removeChild(state.wrapper.childNodes[0]);
+      canvas.remove();
+      canvas = new p5(props.sketch, state.wrapper)
+      return {
+        ...state,
+        sketch: props.sketch,
+        canvas: canvas
+      };
     }
-    if( this.canvas.myCustomRedrawAccordingToNewPropsHandler ) {
-      this.canvas.myCustomRedrawAccordingToNewPropsHandler(newprops);
+    if( canvas && canvas.myCustomRedrawAccordingToNewPropsHandler ) {
+      canvas.myCustomRedrawAccordingToNewPropsHandler(props);
     }
+    return state;
   }
+
 
   componentWillUnmount() {
-		this.canvas.remove();
+		this.state.canvas.remove();
 	}
 
   render() {
