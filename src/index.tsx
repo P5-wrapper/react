f4c70873ff79d02787df31b37e2911e6583b46aa
James Robb <james@jamesrobb.co.uk>
2020-11-26 14:50:12 +0100

    Implementing a reducer based model for testing purposes

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,45 @@
 import p5 from 'p5';
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC, memo, useEffect, useReducer, useRef, useState } from 'react';
 
 export interface IP5WrapperProps {
   sketch: (p: p5) => void;
 }
 
-let canvas: p5 = null;
+export interface IState {
+  canvas: p5;
+}
+
+export interface IAction {
+  type: string;
+  canvas: p5;
+}
+
+function reducer(state: IState, action: IAction) {
+  switch (action.type) {
+    case "SET_CANVAS":
+      return { ...state, canvas: action.canvas };
+    default:
+      return state;
+  }
+}
 
 const P5Wrapper: FC<IP5WrapperProps> = ({ sketch, children }) => {
   const wrapper = useRef(null);
+  const [state, dispatch] = useReducer(reducer, {
+    canvas: null
+  });
 
   useEffect(() => {
-    canvas = new p5(sketch, wrapper.current);
-    return () => canvas && canvas.remove();
+    if (state.canvas !== null) state.canvas.remove();
+    const canvas = new p5(sketch, wrapper.current);
+    dispatch({ type: "SET_CANVAS", canvas });
   }, [sketch]);
 
   return <div ref={wrapper}>{children}</div>;
 }
 
-export default P5Wrapper;
+function shouldRerender(prevProps: IP5WrapperProps, nextProps: IP5WrapperProps) {
+  return prevProps.sketch === nextProps.sketch;
+}
+
+export default memo(P5Wrapper, shouldRerender);
