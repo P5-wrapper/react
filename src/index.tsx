ecf0bcf46aec9aceb69457110384269c1b4650b7
James Robb <james@jamesrobb.co.uk>
2020-12-07 00:45:12 +0100

    Testing new build setups

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import p5 from 'p5';
-import React, { FC, memo, useEffect, useReducer, useRef, useState } from 'react';
-
-type TAttributes = { [key: string]: string };
+import React, { createRef, FC, memo, RefObject, useEffect, useReducer } from 'react';
 
 export interface IP5WrapperProps {
   sketch: (p: p5) => void;
-  attributes: TAttributes;
+  attributes: { [key: string]: string };
 }
 
 export interface IState {
-  canvas: p5;
+  canvas?: p5;
 }
 
 export interface IAction {
   type: string;
-  canvas: p5;
+  canvas?: p5;
 }
 
 function reducer(state: IState, action: IAction) {
@@ -26,23 +24,23 @@ function reducer(state: IState, action: IAction) {
   }
 }
 
-const P5Wrapper: FC<IP5WrapperProps> = ({ sketch, children }) => {
-  const wrapper = useRef(null);
-  const [state, dispatch] = useReducer(reducer, {
-    canvas: null
-  });
-
-  useEffect(() => {
-    if (state.canvas !== null) state.canvas.remove();
-    const canvas = new p5(sketch, wrapper.current);
-    dispatch({ type: "SET_CANVAS", canvas });
-  }, [sketch]);
-
-  return <div ref={wrapper}>{children}</div>;
-}
-
 function shouldRerender(prevProps: IP5WrapperProps, nextProps: IP5WrapperProps) {
   return prevProps.sketch === nextProps.sketch;
 }
 
-export default memo(P5Wrapper, shouldRerender);
+const P5WrapperComponent: FC<IP5WrapperProps> = ({ sketch, children, attributes }) => {
+  const wrapper: RefObject<HTMLDivElement> = createRef();
+  const [state, dispatch] = useReducer(reducer, {});
+
+  useEffect(() => {
+    if (state.canvas && wrapper.current) {
+      state.canvas.remove();
+      const canvas = new p5(sketch, wrapper.current);
+      dispatch({ type: "SET_CANVAS", canvas });
+    }
+  }, [sketch]);
+
+  return <div {...attributes} ref={wrapper}>{children}</div>;
+}
+
+export const P5Wrapper = memo(P5WrapperComponent, shouldRerender);
