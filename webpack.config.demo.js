8199acf5a42ed14d9942e79e21a6b8848915f433
Eugene Dyko <yevdyko@gmail.com>
2021-03-30 14:27:55 +0200

    Build Demo into Dist Folder (#80)
    * Format webpack config
    
    * Build demo into dist folder
    
    * Update deploy script with new build directory path
    
    * Remove example build directory from ignore files
    
    * Use space indent in the .editorconfig
    
    * Use double quotes

--- webpack.config.demo.js
+++ webpack.config.demo.js
@@ -1,38 +1,41 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, "./example/src/index.html"),
-    filename: "./index.html"
+  template: path.join(__dirname, "./example/src/index.html"),
+  filename: "./index.html"
 });
 
 module.exports = {
-
   entry: path.join(__dirname, "./example/src/app.jsx"),
   output: {
-      path: path.join(__dirname, "example/dist"),
-      filename: "bundle.js"
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
   },
   module: {
     rules: [
-        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-        { test: /\.tsx?$/, loader: "ts-loader" },
-        {
-          test: /\.(js|jsx)$/,
-          use: "babel-loader",
-          exclude: /node_modules/
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        }
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ],
+      }
     ]
-},
+  },
   plugins: [htmlWebpackPlugin],
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx", ".js", ".json"]
-},
+  },
   devServer: {
-      port: 3001
+    port: 3001
   },
 };
