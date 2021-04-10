4a3fb5d196b2de9de3a23ae03c95307683246e7f
Eugene Dyko <yevdyko@gmail.com>
2021-04-10 12:50:59 +0200

    Add jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"]
+};
