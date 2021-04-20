384f66142be83af2ec4feb7c320de7aa8e858af9
Eugene Dyko <yevdyko@gmail.com>
2021-04-20 23:27:15 +0200

    Render P5Wrapper component in test

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,5 +1,15 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import P5Wrapper from "./index";
+import sketch from "./__fixtures__/sketch";
+
 describe("P5Wrapper", () => {
-  it("tests the component", () => {
-    // test case should be here
+  xit("renders the component", () => {
+    const props = {
+      sketch: sketch,
+      attributes: { rotation: "10" }
+    };
+
+    render(<P5Wrapper {...props} />);
   });
 });
