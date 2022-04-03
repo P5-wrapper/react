2c6770a52278fe5832823a85fe36013180e6b075
James Robb <47126579+jamesrweb@users.noreply.github.com>
2022-04-03 14:46:27 +0200

    Add generic prop type support (#152)

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,48 @@
 import diff from "microdiff";
 import p5 from "p5";
-import React, { createRef, FC, memo, MutableRefObject, useRef } from "react";
+import React, { createRef, memo, MutableRefObject, useRef } from "react";
 import { useIsomorphicEffect } from "rooks";
 
 type Wrapper = HTMLDivElement;
-export type Sketch = (instance: P5CanvasInstance) => void;
-export type SketchProps = {
-  [key: string]: any;
+type WithChildren<T = unknown> = T & { children?: React.ReactNode };
+type InputProps<Props extends SketchProps = SketchProps> = Props & {
+  sketch: Sketch<Props>;
 };
-export type P5WrapperProps = SketchProps & {
-  sketch: Sketch;
-};
-export type P5CanvasInstance = p5 & {
-  updateWithProps?: (props: SketchProps) => void;
+export type Sketch<Props extends SketchProps = SketchProps> = (
+  instance: P5CanvasInstance<Props>
+) => void;
+export type SketchProps = { [key: string]: unknown };
+export type P5WrapperProps<Props extends SketchProps = SketchProps> =
+  WithChildren<InputProps<Props>>;
+export type P5CanvasInstance<Props extends SketchProps = SketchProps> = p5 & {
+  updateWithProps?: (props: Props) => void;
 };
 
 // @TODO: remove in next major version, keep for compatibility reasons for now.
-export type P5Instance = P5CanvasInstance;
+export type P5Instance<Props extends SketchProps = SketchProps> =
+  P5CanvasInstance<Props>;
 
-function createCanvasInstance(
-  sketch: Sketch,
+function createCanvasInstance<Props extends SketchProps = SketchProps>(
+  sketch: Sketch<Props>,
   wrapper: Wrapper
-): P5CanvasInstance {
+): P5CanvasInstance<Props> {
   return new p5(sketch, wrapper);
 }
 
-function removeCanvasInstance(
-  canvasInstanceRef: MutableRefObject<P5CanvasInstance | undefined>
+function removeCanvasInstance<Props extends SketchProps = SketchProps>(
+  canvasInstanceRef: MutableRefObject<P5CanvasInstance<Props> | undefined>
 ) {
   canvasInstanceRef.current?.remove();
   canvasInstanceRef.current = undefined;
 }
 
-const ReactP5WrapperComponent: FC<P5WrapperProps> = ({
+function ReactP5WrapperComponent<Props extends SketchProps = SketchProps>({
   sketch,
   children,
   ...props
-}) => {
+}: P5WrapperProps<Props>) {
   const wrapperRef = createRef<Wrapper>();
-  const canvasInstanceRef = useRef<P5CanvasInstance>();
+  const canvasInstanceRef = useRef<P5CanvasInstance<Props>>();
 
   useIsomorphicEffect(() => {
     if (wrapperRef.current === null) {
@@ -53,16 +57,33 @@ const ReactP5WrapperComponent: FC<P5WrapperProps> = ({
   }, [sketch]);
 
   useIsomorphicEffect(
-    () => canvasInstanceRef.current?.updateWithProps?.(props),
+    /**
+     * The `as any` cast is begrudgingly required due to a known limitation of the TypeScript compiler as demonstrated in issues:
+     *
+     * - https://github.com/microsoft/TypeScript/issues/35858
+     * - https://github.com/microsoft/TypeScript/issues/37670
+     *
+     * Potentially this will be resolved by this PR once it is eventually merged:
+     *
+     * - https://github.com/microsoft/TypeScript/pull/42382
+     *
+     * Either way, until a resolution is merged into the TypeScript compiler that addresses this issue, we need to use this workaround.
+     * We could also remove this if we manage find a reasonable, more fitting workaround of some sort to avoid casting in the first place.
+     * If a workaround / change of implementation comes to mind, please raise an issue on the repository or feel free to open a PR!
+     */
+    () => canvasInstanceRef.current?.updateWithProps?.(props as any),
     [props]
   );
 
   useIsomorphicEffect(() => () => removeCanvasInstance(canvasInstanceRef), []);
 
   return <div ref={wrapperRef}>{children}</div>;
-};
+}
 
-function propsAreEqual(previous: P5WrapperProps, next: P5WrapperProps) {
+function propsAreEqual<Props extends SketchProps = SketchProps>(
+  previous: P5WrapperProps<Props>,
+  next: P5WrapperProps<Props>
+) {
   const differences = diff(previous, next);
 
   return differences.length === 0;
