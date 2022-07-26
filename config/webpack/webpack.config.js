92809270331f3b9ad217bdff2c47214d306e2e39
James Robb <47126579+jamesrweb@users.noreply.github.com>
2022-07-26 11:37:37 +0200

    Add automated deployments of the demo application on GH Pages (#174)
    * Add automated deployments of the demo application on GH Pages
    
    * Update project name in docs and apply changes to show change successfully deployed

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -5,53 +5,51 @@ const BASE_DIR = join(__dirname, "..", "..");
 const DISTRIBUTION_DIRECTORY = join(BASE_DIR, "dist", "demo");
 const EXAMPLE_DIRECTORY = join(BASE_DIR, "example");
 
-module.exports = (env, options) => {
-  return {
-    entry: join(EXAMPLE_DIRECTORY, "app.jsx"),
-    output: {
-      path: DISTRIBUTION_DIRECTORY,
-      filename: "[name].[contenthash].js"
-    },
-    devServer: {
-      compress: true,
-      port: 3001
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: "ts-loader",
+module.exports = {
+  entry: join(EXAMPLE_DIRECTORY, "app.jsx"),
+  output: {
+    path: DISTRIBUTION_DIRECTORY,
+    filename: "[name].[contenthash].js"
+  },
+  devServer: {
+    compress: true,
+    port: 3001
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          configFile: join(__dirname, "..", "typescript", "tsconfig.json")
+        }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
           options: {
-            configFile: join(__dirname, "..", "typescript", "tsconfig.json")
+            presets: [
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+              "@babel/preset-env"
+            ]
           }
         },
-        {
-          test: /\.(js|jsx)$/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/preset-react",
-                "@babel/preset-typescript",
-                "@babel/preset-env"
-              ]
-            }
-          },
-          exclude: /node_modules/
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"]
-        }
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: join(EXAMPLE_DIRECTORY, "index.html")
-      })
-    ],
-    resolve: {
-      extensions: [".js", ".jsx", ".ts", ".tsx"]
-    }
-  };
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: join(EXAMPLE_DIRECTORY, "index.html")
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  }
 };
