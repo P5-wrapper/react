08e513431272a424d029ea394e4c431d54a64d9e
James Robb <47126579+jamesrweb@users.noreply.github.com>
2022-08-27 14:41:35 +0200

    Update project types and add a guard wrapper to log an appropriate error message if no sketch is provided at runtime. (#177)

--- config/eslint/eslint.config.js
+++ config/eslint/eslint.config.js
@@ -6,7 +6,6 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
     "plugin:react-hooks/recommended",
     "plugin:react/recommended"
   ],
@@ -18,7 +17,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module"
   },
-  plugins: ["@typescript-eslint", "prettier", "react", "simple-import-sort"],
+  plugins: ["@typescript-eslint", "react", "simple-import-sort"],
   overrides: [
     {
       files: [".js", ".jsx", ".ts", ".tsx"]
@@ -28,10 +27,7 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "linebreak-style": ["error", "unix"],
     "simple-import-sort/exports": "error",
-    "simple-import-sort/imports": "error",
-    indent: ["error", 2],
-    quotes: ["error", "double"],
-    semi: ["error", "always"]
+    "simple-import-sort/imports": "error"
   },
   settings: {
     react: {
--- tests/helpers/streams.ts
+++
@@ -1,8 +0,0 @@
-export function unwrapReadableStream(stream: NodeJS.ReadableStream) {
-  return new Promise(resolve => {
-    let data = "";
-
-    stream.on("data", chunk => (data += chunk));
-    stream.on("end", () => resolve(data));
-  });
-}
