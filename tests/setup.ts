886820b1702ef5d1a1a5352f3aa3b7247173eb6b
James Robb <47126579+jamesrweb@users.noreply.github.com>
2023-08-29 02:58:27 +0200

    fix tests and update all dependencies to latest versions

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,7 +1,4 @@
-import matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
-import { afterEach, expect } from "vitest";
-
-expect.extend(matchers);
+import { afterEach } from "vitest";
 
 afterEach(() => cleanup());
