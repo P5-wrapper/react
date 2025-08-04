cf00354e2cffd07086d21dab0f0c6491c068febf
James Robb <47126579+jamesrweb@users.noreply.github.com>
2025-08-04 20:16:17 +0200

    Add p5.record example (#520)

---
+++ demo/sketches/record.jsx
@@ -0,0 +1,43 @@
+import * as record from "p5.record.js";
+
+export function sketch(p5) {
+  let recording;
+
+  p5.setup = () => {
+    const canvas = p5.createCanvas(400, 400);
+    p5.background(200);
+
+    recording = new record.Recorder({
+      frameRate: 60,
+      source: canvas.elt
+    });
+
+    const startBtn = p5.select("#start-recording");
+    startBtn.mouseClicked(() => recording.start());
+
+    const stopBtn = p5.select("#stop-recording");
+    stopBtn.mouseClicked(() => recording.stop());
+
+    const pauseBtn = p5.select("#pause-recording");
+    pauseBtn.mouseClicked(() => recording.pause());
+
+    const resumeBtn = p5.select("#resume-recording");
+    resumeBtn.mouseClicked(() => recording.resume());
+  };
+
+  p5.draw = () => {
+    p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+  };
+
+  p5.keyPressed = () => {
+    if (p5.key === "a") {
+      recording.start();
+    } else if (p5.key === "s") {
+      recording.stop();
+    } else if (p5.key === "d") {
+      recording.pause();
+    } else if (p5.key === "f") {
+      recording.resume();
+    }
+  };
+}
