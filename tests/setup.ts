2078d8475cd9478908c365a169bc13a440fd3e0c
James Robb <47126579+jamesrweb@users.noreply.github.com>
2025-08-16 23:54:43 +0200

    Use happy-dom over jsdom for more performant UI tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,7 +4,7 @@ import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 import "vitest-canvas-mock";
 
-// Disables P5 "friendly errors" to avoid JSDOM-related script scanning that causes unhandled rejections in the tests.
+// Disables P5 "friendly errors" to avoid DOM-related script scanning that causes unhandled rejections in the tests.
 // @see https://p5js.org/reference/p5/disableFriendlyErrors/
 p5.disableFriendlyErrors = true;
 
